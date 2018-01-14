d1e461fca4993eaaa0a0573100c3523782de1716
D-Baze <wadjakorn.t@mvisioncorp.com>
2018-01-14 14:07:15 +0700

    initial project

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>baanjane.com</title>
+<script src="thirdparty/jquery/jquery-3.2.1.min.js"></script>
+<script src="thirdparty/bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="thirdparty/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="css/styles.css" />
+</head>
+<body>
+    <div class="top-nav " >
+        <div class="container">
+            <div class="row" >
+                <div class="col-sm-12">
+                    top-nav
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="header-section container" >
+        <div class="row" >
+            <div class="col-sm-12">
+                header-section
+            </div>
+        </div>
+    </div>
+    <div class="main-menu" >
+        <div class="container" >
+            <div class="row" >
+                <div class="col-sm-12">
+                    main-menu
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="swiper-banner-wrapper" >
+        <div class="container" >
+            <div class="row" >
+                <div class="col-sm-12">
+                    swiper-banner
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
