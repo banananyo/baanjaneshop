1c06a7a3a9a5ffc3a6f79ff5deb73e88c52ca0ee
Wuthiwat Sornso <iboommm@live.com>
2018-01-31 23:42:03 +0700

    update search

--- admin/module/page/manage.php
+++ admin/module/page/manage.php
@@ -4,12 +4,15 @@
 
 <section class="content">
 <div class="box">
+<form action="" method="get">
             <div class="box-header">
               <h3 class="box-title">จัดการหน้าเสริม</h3>
 
               <div class="box-tools">
                 <div class="input-group input-group-sm" style="width: 150px;">
-                  <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+                <input type="hidden" name="module" value="category">
+                 <input type="hidden" name="mode" value="manage">
+                  <input type="text" name="filter" class="form-control pull-right" placeholder="Search">
 
                   <div class="input-group-btn">
                     <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
@@ -17,6 +20,7 @@
                 </div>
               </div>
             </div>
+            </form>
             <!-- /.box-header -->
             <div class="box-body table-responsive no-padding">
               <table class="table table-hover">
