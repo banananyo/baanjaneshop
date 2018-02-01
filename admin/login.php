cfd8628830eb8db3c9c3874129aedb0964f6d3bd
Wuthiwat Sornso <iboommm@live.com>
2018-02-01 10:09:01 +0700

    hoc header

--- admin/login.php
+++ admin/login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>เข้าสู่ระบบ - ระบบจัดการบ้านยา</title>
+    <title>เข้าสู่ระบบ - Baanjane Management System</title>
 
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/font-awesome.min.css" />
